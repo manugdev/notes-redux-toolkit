a0ea522b0311b8fc61f4d85c80ba450b7d5c7106
Manuel Garcia <manuelgarcia24696@gmail.com>
2022-11-04 00:58:33 -0300

    change initial state

--- src/features/task/taskSlice.js
+++ src/features/task/taskSlice.js
@@ -1,23 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = [
-    {
-        id: '1',
-        title: 'Task 1',
-        description: 'Task 1 description',
-        completed: false
-    },
-    {
-        id: '2',
-        title: 'Task 2',
-        description: 'Task 2 description',
-        completed: false
-    },
-]
-
 export const taskSlice = createSlice({
     name: 'task',
-    initialState,
+    initialState: [],
     reducers: {
         addTask: (state, action) =>{
             state.push(action.payload)
